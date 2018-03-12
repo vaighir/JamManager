1cacbfd79175deef07ccb56ff8c5f35b898368ac
vaighir <vaighir@gmail.com>
2018-03-12 16:18:04 +0100

    added some new cities

--- src/main/resources/META-INF/sql/data.sql
+++ src/main/resources/META-INF/sql/data.sql
@@ -9,8 +9,18 @@ insert into instruments(name) value("keyboard");
 insert into cities(name) value("Warszawa");
 insert into cities(name) value("Wrocław");
 insert into cities(name) value("Kraków");
+insert into cities(name) value("Łódź");
+insert into cities(name) value("Gdańsk");
+insert into cities(name) value("Katowice");
+
 insert into cities(name) value("Berlin");
+insert into cities(name) value("Hamburg");
+insert into cities(name) value("Bremen");
 insert into cities(name) value("Dresden");
+insert into cities(name) value("Leipzig");
+insert into cities(name) value("Frankfurt");
+insert into cities(name) value("Wuppertal");
+insert into cities(name) value("Köln");
 insert into cities(name) value("Düsseldorf");
 
-
+insert into cities(name) value("Praha");
