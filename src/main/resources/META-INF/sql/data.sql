89bcb7ddcf865d7efb1638cd7b415ac3b26551bf
vaighir <vaighir@gmail.com>
2018-03-12 15:51:17 +0100

    created persistence.xml

---
+++ src/main/resources/META-INF/sql/data.sql
@@ -0,0 +1,16 @@
+insert into roles(name) value("admin");
+insert into roles(name) value("user");
+
+insert into instruments(name) value("drums");
+insert into instruments(name) value("guitar");
+insert into instruments(name) value("bass");
+insert into instruments(name) value("keyboard");
+
+insert into cities(name) value("Warszawa");
+insert into cities(name) value("Wrocław");
+insert into cities(name) value("Kraków");
+insert into cities(name) value("Berlin");
+insert into cities(name) value("Dresden");
+insert into cities(name) value("Düsseldorf");
+
+
