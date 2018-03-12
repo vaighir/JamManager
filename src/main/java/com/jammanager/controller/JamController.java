1846b87343054f29efb3a13fce4324f4432e7c35
vaighir <vaighir@gmail.com>
2018-03-12 19:21:45 +0100

    added jamcontroller methods

--- src/main/java/com/jammanager/controller/JamController.java
+++ src/main/java/com/jammanager/controller/JamController.java
@@ -1,8 +1,118 @@
 package com.jammanager.controller;
 
+import java.util.Collection;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.jammanager.entity.Comment;
+import com.jammanager.entity.Jam;
+import com.jammanager.repository.CommentRepository;
+import com.jammanager.repository.JamRepository;
 
 @Controller
 public class JamController {
+
+//	@Autowired
+//	private UserRepository userRepository;
+
+	@Autowired
+	private JamRepository jamRepository;
+
+//	@Autowired
+//	private CityRepository cityRepository;
+
+	@Autowired
+	private CommentRepository commentRepository;
+
+	@GetMapping(path = "/jam/all")
+	public String showAllJams(Model model) {
+		Collection<Jam> jams = jamRepository.findAll();
+
+		model.addAttribute("jams", jams);
+		return "jam/all";
+	}
+
+	@GetMapping(path = "/jam/add")
+	public String showAddJamForm(Model model) {
+
+		Jam jam = new Jam();
+		model.addAttribute("jam", jam);
+
+		return "jam/add";
+	}
+
+	@PostMapping(path = "/jam/add")
+	public String processAddJamForm(@Valid Jam jam, BindingResult bresult) {
+
+		if (bresult.hasErrors()) {
+			return "jam/add";
+		}
+
+		jamRepository.save(jam);
+
+		return "redirect:all";
+	}
+
+	@GetMapping(path = "/jam/{id}")
+	public String showJam(@PathVariable(name = "id", required = true) long id, Model model) {
+		Jam jam = jamRepository.findOne(id);
+		Collection<Comment> comments = commentRepository.findAllByJamId(id);
+
+		model.addAttribute("comments", comments);
+		model.addAttribute("jam", jam);
+		return "jam/jam";
+	}
+
+	@PostMapping(path = "/jam/{id}/comment")
+	public String commentJam(@Valid Comment comment, @PathVariable(name = "id", required = true) long id, Model model) {
+		Jam jam = jamRepository.findOne(id);
+
+		commentRepository.save(comment);
+
+		Collection<Comment> comments = commentRepository.findAllByJamId(id);
+
+		model.addAttribute("comments", comments);
+		model.addAttribute("jam", jam);
+		return "jam/jam";
+	}
+	
+	@GetMapping(path = "/jam/delete")
+	public String showDeleteConfirmForm(@RequestParam(name = "id", required = true) long id, Model model) {
+				
+		Jam jam = jamRepository.findOne(id);
+		
+		model.addAttribute("jam", jam);
+
+		return "jam/delete";
+	}
+	
+	@PostMapping(path = "jam/delete")
+	public String deleteJam(final @RequestParam(name = "id", required = true) long id) {
+		
+		jamRepository.delete(id);
+		
+		return "redirect:all";
+	}
+
+//	@ModelAttribute("users")
+//	public Collection<User> users() {
+//		Collection<User> users = userRepository.findAll();
+//		return users;
+//	}
+//
+//	@ModelAttribute("cities")
+//	public Collection<City> cities() {
+//		Collection<City> cities = cityRepository.findAll();
+//		return cities;
+//	}
 
 }
--- src/main/java/com/jammanager/repository/CommentRepository.java
+++ src/main/java/com/jammanager/repository/CommentRepository.java
@@ -1,10 +1,12 @@
 package com.jammanager.repository;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jammanager.entity.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-
+	Collection<Comment> findAllByJamId(long id); 
 }
